b5cb52286b71549b527d39547b5347f530b18e51
jadrog05 <rogersjaden@icloud.com>
2023-05-26 14:29:46 +0100

    first commit

---
+++ StudentManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentManagementSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
